9860d21105c27f3b838332594d7a32ebf9283af6
Derek <derek@MacBook-Air.local>
2020-12-28 22:25:56 -0330

    Day 1

---
+++ running_sum_1d_array.py
@@ -0,0 +1,29 @@
+class Solution:
+  def running_sum(self, nums):
+      running_sum = []
+      running_num = 0
+      
+      for num in nums:
+          running_num += num
+          running_sum.append(running_num)
+          
+      return running_sum
+
+# Test cases
+
+sol = Solution()
+nums1 = [1, 2, 3, 4]
+nums2 = [1, 1, 1, 1, 1]
+nums3 = [3, 1, 2, 10, 1]
+
+print('Input:    [1, 2, 3, 4]')
+print('Output:  ', sol.running_sum(nums1))
+print('Expected: [1, 3, 6, 10]')
+
+print('Input:    [1, 1, 1, 1, 1]')
+print('Output:  ', sol.running_sum(nums2))
+print('Expected: [1, 2, 3, 4, 5]')
+
+print('Input:    [3, 1, 2, 10, 1]')
+print('Output:  ', sol.running_sum(nums3))
+print('Expected: [3, 4, 6, 16, 17]')
