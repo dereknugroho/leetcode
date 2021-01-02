e0aa9fb3a8ae33b9b35819eba49cca31e4e4e893
Derek <derek@MacBook-Air.local>
2021-01-02 01:32:20 -0330

    Running Sum of 1d Array

--- running_sum_1d_array.py
+++ running_sum_1d_array.py
@@ -1,13 +1,13 @@
 class Solution:
   def running_sum(self, nums):
-      running_sum = []
-      running_num = 0
-      
-      for num in nums:
-          running_num += num
-          running_sum.append(running_num)
-          
-      return running_sum
+    running_sum = []
+    running_num = 0
+    
+    for num in nums:
+        running_num += num
+        running_sum.append(running_num)
+        
+    return running_sum
 
 # Test cases
 
