dc91ca850b38fb75f7f061ab5183df0106f20d88
Derek <derek@MacBook-Air.local>
2021-01-02 17:41:06 -0330

    Kids With the Greatest Number of Candies

--- kids_greatest_candies.py
+++ kids_greatest_candies.py
@@ -14,23 +14,23 @@ class Solution:
         return can_have_greatest
 
 sol = Solution()
-candies1 = [2,3,5,1,3]
+candies1 = [2, 3, 5, 1, 3]
 extra1 = 3
-candies2 = [4,2,1,1,2]
+candies2 = [4, 2, 1, 1, 2]
 extra2 = 1
-candies3 = [12,1,12]
+candies3 = [12, 1, 12]
 extra3 = 10
 
-print('Input:    [2,3,5,1,3], 3')
+print('Input:    [2, 3, 5, 1, 3], 3')
 print('Output:  ', sol.kids_with_candies(candies1, extra1))
 print('Expected: [True, True, True, False, True]')
 print('---')
 
-print('Input:    [4,2,1,1,2], 1')
+print('Input:    [4, 2, 1, 1, 2], 1')
 print('Output:  ', sol.kids_with_candies(candies2, extra2))
 print('Expected: [True, False, False, False, False]')
 print('---')
 
-print('Input:    [12,1,12], 10')
+print('Input:    [12, 1, 12], 10')
 print('Output:  ', sol.kids_with_candies(candies3, extra3))
 print('Expected: [True, False, True]')
