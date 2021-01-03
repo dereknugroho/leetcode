9c011684aa202429947872bde9cd5b8cc8927fc9
Derek <derek@MacBook-Air.local>
2021-01-03 02:12:10 -0330

    Goal Parser Interpretation

---
+++ goal_parser.py
@@ -0,0 +1,40 @@
+class Solution:
+    def interpret(self, command):
+        parsed = ''
+        i = 0
+
+        while i < len(command):
+            if command[i] == 'G':
+                parsed += 'G'
+            elif command[i:i + 2] == '()':
+                parsed += 'o'
+            elif command[i:i + 4] == '(al)':
+                parsed += 'al'
+            i += 1
+
+        return parsed
+
+sol = Solution()
+
+command1 = 'G()(al)'
+expected1 = 'Goal'
+
+command2 = 'G()()()()(al)'
+expected2 = 'Gooooal'
+
+command3 = '(al)G(al)()()G'
+expected3 = 'alGalooG'
+
+print(f'Input:    \'{command1}\'')
+print(f'Output:   \'{sol.interpret(command1)}\'')
+print(f'Expected: \'{expected1}\'')
+print('---')
+
+print(f'Input:    \'{command2}\'')
+print(f'Output:   \'{sol.interpret(command2)}\'')
+print(f'Expected: \'{expected2}\'')
+print('---')
+
+print(f'Input:    \'{command3}\'')
+print(f'Output:   \'{sol.interpret(command3)}\'')
+print(f'Expected: \'{expected3}\'')
