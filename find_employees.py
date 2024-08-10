0dcd96de3ecd9727162ed02e56c3f0c7ed855179
Derek <dereknugroho@gmail.com>
2024-08-10 20:22:34 -0300

    Complete more problems

---
+++ find_employees.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
+    merged = employee.merge(employee, left_on='managerId', right_on='id', suffixes=('', '_manager'))
+    result = merged[merged['salary'] > merged['salary_manager']]
+
+    return result[['name']].rename(columns={'name': 'Employee'})
